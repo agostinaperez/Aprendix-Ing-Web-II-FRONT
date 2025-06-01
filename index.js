8f871d6023fe8525726e94710692fa82bbe7fcc8
CamiDoffo <116676956+CamiDoffo@users.noreply.github.com>
2025-06-01 20:14:50 -0300

    arreglando links de index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const courses = [
-    { title: 'Desarrollo Web', description: 'Aprende HTML, CSS, JavaScript y más.', img: "../resources/cursoWeb.png"},
-    { title: 'Diseño UX/UI', description: 'Crea experiencias digitales efectivas.', img: "../resources/cursoUXUI.png"},
-    { title: 'Marketing Digital', description: 'Domina estrategias de marketing online.', img: "../resources/cursoMarketing.png"},
-    { title: 'Python para Principiantes', description: 'Aprende desde cero practicando.', img: "../resources/cursoPython.png"},
-    { title: 'Gestión de Proyectos', description: 'Planifica y lidera proyectos con éxito', img: "../resources/cursoGestProy.png"},
-    { title: 'Introducción a JavaScript', description: 'Aprende los fundamentos de JavaScript desde cero.', img: "../resources/cursoJava.jpg"},
-    { title: 'React Avanzado', description: 'Domina React y crea aplicaciones web modernas.', img: "../resources/cursoReact.jpg"},
-    { title: 'Python para Ciencia de Datos', description: 'Analiza datos y crea modelos predictivos con Python.', img: "../resources/cursoData.jpg"}
+    { title: 'Desarrollo Web', description: 'Aprende HTML, CSS, JavaScript y más.', img: "resources/cursoWeb.png"},
+    { title: 'Diseño UX/UI', description: 'Crea experiencias digitales efectivas.', img: "resources/cursoUXUI.png"},
+    { title: 'Marketing Digital', description: 'Domina estrategias de marketing online.', img: "resources/cursoMarketing.png"},
+    { title: 'Python para Principiantes', description: 'Aprende desde cero practicando.', img: "resources/cursoPython.png"},
+    { title: 'Gestión de Proyectos', description: 'Planifica y lidera proyectos con éxito', img: "resources/cursoGestProy.png"},
+    { title: 'Introducción a JavaScript', description: 'Aprende los fundamentos de JavaScript desde cero.', img: "resources/cursoJava.jpg"},
+    { title: 'React Avanzado', description: 'Domina React y crea aplicaciones web modernas.', img: "resources/cursoReact.jpg"},
+    { title: 'Python para Ciencia de Datos', description: 'Analiza datos y crea modelos predictivos con Python.', img: "resources/cursoData.jpg"}
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -29,7 +29,7 @@ function cursos(){
         scrollCursos.appendChild(curso);
     }
     const vermas = document.createElement('a');
-    vermas.href = "../cursos/cursos.html";
+    vermas.href = "cursos/cursos.html";
     vermas.className = "btn btn-primary";
     vermas.innerHTML = `Ver más`;
     scrollCursos.appendChild(vermas);
